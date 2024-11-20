0b91193427368e588ec62a99fac8574963859b9b
joy935 <tupaiv@gmail.com>
2024-11-20 21:30:29 +0100

    added error handling

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use((req, res, next) => {
     next();
 });
 
+process.on('uncaughtException', (err, origin) => {
+    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+  });
+
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
